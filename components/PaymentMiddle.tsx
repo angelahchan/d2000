b62dce6d157a2fcec1ad749ee124db571ffe4ad5
Angela Chan <z5210294@ad.unsw.edu.au>
2020-07-27 16:48:19 +1000

    start payment modal

--- components/PaymentMiddle.tsx
+++ components/PaymentMiddle.tsx
@@ -1,26 +1,44 @@
 import * as React from 'react';
-import { StyleSheet, Image, ScrollView,Dimensions} from 'react-native';
+import { StyleSheet, Image, ScrollView,Dimensions, TouchableOpacity, Modal, Button} from 'react-native';
 import * as PSTATE from '../constants/PaymentState';
 import * as COL from '../constants/MainColors';
 import { Ionicons } from '@expo/vector-icons';
 import { Text, View} from '../components/Themed';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 export default function PaymentMiddle(props:any){
     let  paymentStat = props.paymentStat;
     let setCanPay = props.setCanPay;
-
+    //payment form to add a new card
+    const [modalVisible, setModalVisible] = React.useState(false);
     return (
-
     <View style={styles.scrollbox} >
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+            >   
+                <View style={styles.modal}>
+                    <TouchableOpacity
+                        style={styles.closeBtn} 
+                        onPress={() => {
+                        setModalVisible(!modalVisible);
+                    }}>
+                        <AddIcon name="ios-close" color="gray"/>
+                    </TouchableOpacity>
+                <Text>
+                    Hellooooo
+                </Text>
+                </View>
+            </Modal>  
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}
           >
             <View style={styles.cardContainer}>
-                <View style={styles.paymentBox}>
+                <TouchableOpacity style={styles.paymentBox} onPress={() => {
+                setModalVisible(!modalVisible);}
+                }>
                     <Text>Add a Payment Method</Text>
                     <AddIcon name="ios-add" color="grey" />
-                </View>
-                <View style={styles.paymentBox}></View>
-                <View style={styles.paymentBox}></View>
-                <View style={styles.paymentBox}></View>
+                </TouchableOpacity>
 
             </View>
             
@@ -31,7 +49,8 @@ export default function PaymentMiddle(props:any){
 function AddIcon(props: { name: string; color: string }) {
     return <Ionicons size={50}  {...props} />;
   }
-  let width = Dimensions.get('window').width;
+
+
 const styles = StyleSheet.create({
     
     scrollbox: {
@@ -70,6 +89,22 @@ const styles = StyleSheet.create({
         alignItems:'flex-start',
         flexDirection:'row',
 
+    },
+    modal:{
+        zIndex: 1, /* Sit on top */
+        paddingTop: 100,/* Location of the box */
+        left: 0,
+        top: 0,
+        width: '100%',/* Full width */
+        height: '100%', /* Full height */
+        overflow: 'hidden', /* Enable scroll if needed */
+        backgroundColor: 'rgb(255,255,255)', /* Fallback color */
+    },
+    closeBtn:{
+        alignSelf: 'flex-end',
+        padding:10,
+        position: 'absolute',
+        right:5
     }
 
   });
